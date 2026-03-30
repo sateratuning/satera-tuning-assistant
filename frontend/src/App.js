1764ae42ba28164e3da9dbf1f6623714c29d5ac5
sateratuning <info@sateratuning.com>
2026-03-30 11:27:43 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainApp from './MainApp';
 import TrainerMode from './TrainerMode';
 import LogComparison from './LogComparison';
+import Portal from './Portal';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        {/* specific routes first */}
-        <Route path="/trainer" element={<TrainerMode />} />
+        <Route path="/trainer"        element={<TrainerMode />} />
         <Route path="/log-comparison" element={<LogComparison />} />
-        {/* catch-all: "/" and everything else -> MainApp */}
-        <Route path="*" element={<MainApp />} />
+        <Route path="/portal"         element={<Portal />} />
+        <Route path="*"               element={<MainApp />} />
       </Routes>
     </Router>
   );
